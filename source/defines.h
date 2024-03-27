ca7454a6aa798ab53a084df860f77d14cd6d5bb5
Андрей Степанов <therain3164@yandex.ru>
2024-03-27 16:35:34 +0300

    base

---
+++ source/defines.h
@@ -0,0 +1,5 @@
+#define WINDOW_TITLE "Best Game"
+#define FPS 20
+
+#define WINDOW_WIDTH 900
+#define WINDOW_HEIGHT 900
--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,28 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "defines.h"
+using namespace std;
 
 int main(){
-    std::cout<<"Hello world!";
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
+    sf::Vector2f bgSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    sf::Texture background;
+    background.loadFromFile("textures/background.png");
+    sf::Sprite spriteBG;
+    spriteBG.setTexture(background);
+    spriteBG.setPosition(0, 0);
+    spriteBG.setScale(
+            bgSize.x / spriteBG.getLocalBounds().width,
+            bgSize.y / spriteBG.getLocalBounds().height);
+    window.setFramerateLimit(FPS);
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(spriteBG);
+
+        window.display();
+    }
 }
